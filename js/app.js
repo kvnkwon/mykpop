d485d015e5674b6e4a044ba838091c60f3021ee1
Kevin <kvnkwon@gmail.com>
2015-06-19 16:51:41 -0400

    Add slick

--- js/app.js
+++ js/app.js
@@ -1,4 +1,12 @@
 $(document).ready(function() {
   $('.covervid-video').coverVid(1920, 1080);
+  $('.your-class').slick({
+    infinite: true,
+    dots: true,
+    speed: 500,
+    slidesToShow: 1,
+    fade: true,
+    cssEase: 'linear'
+  });
 
 });
